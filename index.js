9e38f261079f5e9eb5bd16d42205de69c4d17abf
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-24 15:36:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => (renderBooks(json)));
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector("main");
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function() {
+  fetchBooks();
+});
